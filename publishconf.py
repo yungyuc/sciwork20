06c67515e9d7a965efb8ce2743fa7c5b68985009
Yung-Yu Chen <yyc@solvcon.net>
2019-09-26 11:23:13 +0800

    Make SITEURL optional

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://conf.sciwork.org/2020'
+SITEURL = os.environ.get('SITEURL', 'https://conf.sciwork.dev/2020')
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
